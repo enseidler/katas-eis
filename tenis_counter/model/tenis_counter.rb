e40db4fc4fe5b7a9522c501e46ffb3d45517e25a
Ezequiel <enseidler@gmail.com>
2015-04-25 13:40:34 -0300

    TenisCounter - Test 01

---
+++ tenis_counter/model/tenis_counter.rb
@@ -0,0 +1,15 @@
+class TenisCounter
+
+    def initialize()
+        @sets = {"jugador_1" =>0, "jugador_2" => 0}
+    end
+
+    def sets
+        @sets
+    end
+
+    def setsGoes(sets_1, sets_2)
+        sets_1 == sets["jugador_1"] && sets_2 == sets["jugador_2"]
+    end
+
+end
---
+++ tenis_counter/spec/tenis_counter_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require_relative '../model/tenis_counter.rb'
+
+describe 'TenisCounter'  do
+   
+    it 'sets counter should be (0,0) when the match begins' do
+        counter = TenisCounter.new
+        counter.setsGoes(0,0).should be true
+    end
+    
+end
