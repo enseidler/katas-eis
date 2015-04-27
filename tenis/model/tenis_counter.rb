729a97180976eaebabbb4711aaa5b4699ff0e135
Ezequiel <enseidler@gmail.com>
2015-04-27 00:05:14 -0300

    Replanteamiento de implementacion - TenisCounter Test 01

--- tenis/model/tenis_counter.rb
+++ tenis/model/tenis_counter.rb
@@ -1,33 +1,20 @@
 class TenisCounter
+    
+    #Indexes for players
+    PLAYER_1 = 0 
+    PLAYER_2 = 1
 
     def initialize()
-        @sets = {"jugador_1" =>0, "jugador_2" => 0}
-        @game_points = {"jugador_1" =>0, "jugador_2" => 0}
-        @games = {"jugador_1" =>0, "jugador_2" => 0}
+        @sets = [0,0] # Number of sets for each player
     end
 
     def sets
         @sets
     end
 
-    def game_points
-        @game_points
-    end
-
-    def games
-        @games
-    end
-
+    #Check score sets in the match, for both players
     def match_goes(sets_1, sets_2)
-        sets_1 == sets["jugador_1"] && sets_2 == sets["jugador_2"]
-    end
-
-    def game_goes(points_1, points_2)
-        points_1 == game_points["jugador_1"] && points_2 == game_points["jugador_2"]
-    end
-    
-    def set_goes(games_1, games_2)
-        games_1 == games["jugador_1"] && games_2 == games["jugador_2"]
+        sets_1 == sets[PLAYER_1] && sets_2 == sets[PLAYER_2]
     end
 
 end
