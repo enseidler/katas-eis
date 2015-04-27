4606159dabba44214170fed743123fc0314c506c
Ezequiel <enseidler@gmail.com>
2015-04-27 00:12:28 -0300

    TenisCounter Test 02

--- tenis/model/tenis_counter.rb
+++ tenis/model/tenis_counter.rb
@@ -6,15 +6,25 @@ class TenisCounter
 
     def initialize()
         @sets = [0,0] # Number of sets for each player
+        @games = [0,0] # Number of games for each player
     end
 
     def sets
         @sets
     end
 
-    #Check score sets in the match, for both players
+    def games
+        @games
+    end
+
+    #Check score sets in the current match, for both players
     def match_goes(sets_1, sets_2)
         sets_1 == sets[PLAYER_1] && sets_2 == sets[PLAYER_2]
+    end
+
+    #Check score games in the current set, for both players
+    def set_goes(games_1, games_2)
+        games_1 == games[PLAYER_1] && games_2 == games[PLAYER_2]
     end
 
 end
--- tenis/spec/tenis_counter_spec.rb
+++ tenis/spec/tenis_counter_spec.rb
@@ -3,9 +3,14 @@ require_relative '../model/tenis_counter.rb'
 
 describe 'TenisCounter'  do
    
-    it 'sets counter should be (0 - 0) when the match begins' do
+    it 'sets counter should be (0-0) when the match begins' do
         counter = TenisCounter.new
         counter.match_goes(0,0).should be true
+    end
+
+    it 'set games counter should be (0-0) when the match begins' do
+        counter = TenisCounter.new
+        counter.set_goes(0,0).should be true
     end
 
 end
