6bb177d9caf39f4f256f1bbab36ec2f7ce924486
Ezequiel <enseidler@gmail.com>
2015-04-27 00:20:28 -0300

    TenisCounter Test 03

--- tenis/model/tenis_counter.rb
+++ tenis/model/tenis_counter.rb
@@ -7,6 +7,7 @@ class TenisCounter
     def initialize()
         @sets = [0,0] # Number of sets for each player
         @games = [0,0] # Number of games for each player
+        @points = [0,0] # Abstraction of the points for each player
     end
 
     def sets
@@ -17,6 +18,10 @@ class TenisCounter
         @games
     end
 
+    def points
+        @points
+    end
+
     #Check score sets in the current match, for both players
     def match_goes(sets_1, sets_2)
         sets_1 == sets[PLAYER_1] && sets_2 == sets[PLAYER_2]
@@ -25,6 +30,11 @@ class TenisCounter
     #Check score games in the current set, for both players
     def set_goes(games_1, games_2)
         games_1 == games[PLAYER_1] && games_2 == games[PLAYER_2]
+    end
+
+    #Check score points in the current game, for both players
+    def game_goes(points_1, points_2)
+        points_1 == points[PLAYER_1] && points_2 == points[PLAYER_2]
     end
 
 end
--- tenis/spec/tenis_counter_spec.rb
+++ tenis/spec/tenis_counter_spec.rb
@@ -13,4 +13,9 @@ describe 'TenisCounter'  do
         counter.set_goes(0,0).should be true
     end
 
+    it 'game points counter should be (0 - 0) when the match begins' do
+        counter = TenisCounter.new
+        counter.game_goes(0,0).should be true
+    end
+
 end
