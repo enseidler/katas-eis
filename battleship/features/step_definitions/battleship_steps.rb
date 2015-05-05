4fb2c5e055db717461016ea539f60143472a8597
Ezequiel <enseidler@gmail.com>
2015-05-05 02:50:55 -0300

    Battleship - Scenario 02 - Create large ship

--- battleship/features/step_definitions/battleship_steps.rb
+++ battleship/features/step_definitions/battleship_steps.rb
@@ -14,7 +14,8 @@ Then(/^position "(.*?)" is not empty$/) do |coord|
   expect(@board.is_empty(row.to_i,column.to_i)).to eq false
 end
 
-Given(/^I create a large ship in position "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^I create a large ship in position "(.*?)"$/) do |coord|
+  row, column = coord.split(":")
+  @board.put_large_ship(row.to_i,column.to_i)
 end
 
