0d3542d2251e00433fad8bd6c137092fa2bf3860
Ezequiel <enseidler@gmail.com>
2015-05-24 19:06:29 -0300

    Scenario passed - Create a small ship in a valid location

--- battleship/app/app.rb
+++ battleship/app/app.rb
@@ -13,7 +13,29 @@ module Battleship
 
     post 'start_battle' do
       @board = Board.new(params[:rows].to_i, params[:columns].to_i)
+      @last_event = 'Battleship board succefully created. Size ('+@board.rows.to_s+'x'+@board.rows.to_s+')'
       session[:board] = @board
+      render 'battle/battle'      
+    end
+
+    post 'create_small' do
+      begin
+        session[:board].put_small_ship(params[:in_row].to_i, params[:in_column].to_i)
+        @last_event = 'Ship succefully created in ('+params[:in_row].to_s+','+params[:in_column].to_s+')'
+      rescue RuntimeError => error
+        @last_event = error.message       
+      end
+      @board = session[:board]
+      render 'battle/battle'      
+    end
+
+    post 'check_cell' do
+      if(session[:board].is_empty(params[:row].to_i, params[:column].to_i))
+        @last_event = 'The position ('+params[:row].to_s+','+params[:column].to_s+') is empty'
+      else
+        @last_event = 'The position ('+params[:row].to_s+','+params[:column].to_s+') has a ship'
+      end
+      @board = session[:board]
       render 'battle/battle'      
     end
 
--- battleship/features/step_definitions/battleship_steps.rb
+++ battleship/features/step_definitions/battleship_steps.rb
@@ -1,14 +1,25 @@
 
 Given(/^a board with dimensions "(.*?)" x "(.*?)"$/) do |rows,columns|
   visit '/start'
+  fill_in :rows, :with => rows
+  fill_in :columns, :with => columns
+  click_button 'start_button' 
 end
 
 Given(/^I create a small ship in position "(.*?)"$/) do |coord|
-  pending # express the regexp above with the code you wish you had
+  row, column = coord.split ':'
+  fill_in :in_row, :with => row
+  fill_in :in_column, :with => column
+  click_button 'create_small_button'
 end
 
 Then(/^position "(.*?)" is not empty$/) do |coord|
-  pending # express the regexp above with the code you wish you had
+  row, column = coord.split ':'
+  fill_in :row, :with => row
+  fill_in :column, :with => column
+  click_button 'check_cell_button'
+  expected = 'The position ('+row.to_s+','+column.to_s+') has a ship'
+  expect(page.find('#last_event')).to have_content(expected)
 end
 
 Given(/^I create a large ship in position "(.*?)"$/) do |coord|
